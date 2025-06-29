02582cf103eec94ba49cd992bf02b039762817c8
ViroKalu <virokemin@gmail.com>
2025-06-29 16:39:17 +0530

    UserControlAdded

---
+++ CarSaleManage/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using CarSaleManage.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSaleManage.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public UserController(UserManager<AppUser> userManager) 
+        {
+            this.userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.ToListAsync();
+
+            return View(users);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await userManager.FindByIdAsync(id.ToString()!);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await userManager.FindByIdAsync(id.ToString()!);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Vehicles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString()!);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await userManager.DeleteAsync(user);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
